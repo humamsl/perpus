2297eddec31c4e3a17e52c21de82e2709b9a27b3
Humam Studio <humamfvnky@gmail.com>
2026-07-06 10:54:12 +0700

    fix update

--- storage/framework/views/73966e0fe02b54f0f46ed319aa84e8d2.php
+++
@@ -1,7 +0,0 @@
-<?php $__env->startSection('title','Edit Buku Fisik'); ?>
-<?php $__env->startSection('content'); ?>
-<h1 class="text-2xl font-bold mb-4">Edit Buku Fisik</h1>
-<?php echo $__env->make('offline-books._form', ['action' => route('offline-books.update', $book), 'method' => 'PUT'], array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\laragon\www\Aplikasi Perpus\digital-library\resources\views/offline-books/edit.blade.php ENDPATH**/ ?>
